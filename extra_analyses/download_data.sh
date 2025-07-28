73edb975bd4161a6b7a8c5c2212b9600b098e6e5
Seppe De Winter <seppe.dewinter@hotmail.com>
2025-07-28 08:09:27 +0200

    Create PBMC cisTarget database.

---
+++ extra_analyses/download_data.sh
@@ -0,0 +1,1 @@
+wget https://resources.aertslab.org/CREsted/manuscript_data/crested_data.tar.gz
---
+++ extra_analyses/pycisTarget/download_motif_collection.sh
@@ -0,0 +1,3 @@
+wget https://resources.aertslab.org/cistarget/motif_collections/v10nr_clust_public/v10nr_clust_public.zip
+
+unzip v10nr_clust_public.zip
