bf85a98cafca407c618b5793241a2ad8b54d843f
Seppe De Winter <seppe.dewinter@hotmail.com>
2025-09-15 13:35:45 +0200

    compare motifs from motif enrichment and crested using tfmindi.

--- extra_analyses/compare_pycistarget_CREsted/PBMC/compare_motifs.py
+++ extra_analyses/compare_pycistarget_CREsted/PBMC/compare_motifs.py
@@ -7,6 +7,9 @@ from memelite import tomtom
 import scanpy as sc
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import logomaker
+import tfmindi as tm
 
 sc.settings.figdir = "plots"
 
@@ -64,9 +67,49 @@ def load_motif(n, d):
         motifs.append(tmp)
     return motifs, names
 
+def load_motif_chromvar(p):
+    nuc_index = list("ACGT")
+    motifs = []
+    names = []
+    current_motif = 1
+    with open(p) as f:
+        motif = [[], [], [], []]
+        for line in f:
+            if line.startswith("A:"):
+                motif = np.array(motif)
+                if np.array(motif).shape[1] > 0 and motif.min() >= 0 :
+                    motif = (motif / motif.sum(0))
+                    motifs.append(motif.T)
+                    names.append(f"chromvar_{current_motif}")
+                current_motif += 1
+                motif = [[], [], [], []]
+            if not (
+                line.startswith("A:") or \
+                line.startswith("C:") or \
+                line.startswith("G:") or \
+                line.startswith("T:")
+            ):
+                continue
+            line = line.replace("-inf", "0.0")
+            nuc = line.split()[0].replace(":", "")
+            motif[nuc_index.index(nuc)] = [
+                float(x) for x in line.split()[1:]
+            ]
+    return motifs, names
+
+def ic(ppm, bg=np.array([0.27, 0.23, 0.23, 0.27]), eps=1e-3) -> np.ndarray:
+    return (
+        ppm * np.log(ppm + eps) / np.log(2) - bg * np.log(bg) / np.log(2)
+    ).sum(1)
+
 # Load enriched motifs as ppm
 
 MOTIF_DIR = "../../pycisTarget/v10nr_clust_public/singletons/"
+
+motif_annotations_file = tm.fetch_motif_annotations()
+motif_annotations = tm.load_motif_annotations(motif_annotations_file)
+
+motif_to_db = tm.load_motif_to_dbd(motif_annotations)
 
 enriched_motifs: list[str] = []
 with open("enriched_motifs.txt") as f:
@@ -110,19 +153,28 @@ for f_modisco_res in tqdm(modisco_results):
         modisco_motifs_ic.append(ic)
         modisco_motifs_is_pos.append(is_pos)
 
+# Load chromvar motifs
+
+chromvar_motifs, chromvar_names = load_motif_chromvar(
+    "/data/groups/vib.ai/stein.aerts/dabaffy/crested_revision/enriched_motifs.csv"
+)
 
 all_motifs = [
     ppm.T for ppm in [
         *enriched_motifs_ppm,
-        *modisco_motifs_ppm
+        *modisco_motifs_ppm,
+        *chromvar_motifs
     ]   
 ]
 
-motif_names = [*enriched_motifs_sub_names, *modisco_motifs_names]
+motif_names = [
+    *enriched_motifs_sub_names, *modisco_motifs_names, *chromvar_names
+]
 
 motif_technique = [
     *np.repeat("pycisTarget", len(enriched_motifs_ppm)),
-    *np.repeat("deepPBMC", len(modisco_motifs_ppm))
+    *np.repeat("deepPBMC", len(modisco_motifs_ppm)),
+    *np.repeat("chromVAR", len(chromvar_motifs))
 ]
 
 motif_metadata = pd.DataFrame(
@@ -144,20 +196,67 @@ sc.tl.pca(adata_motifs_dl_ctx)
 sc.pp.neighbors(adata_motifs_dl_ctx)
 sc.tl.tsne(adata_motifs_dl_ctx)
 
+adata_motifs_dl_ctx.obs["dbd"] = [
+    motif_to_db.get(x, np.nan) for x in adata_motifs_dl_ctx.obs.index
+]
+
 sc.pl.tsne(
     adata_motifs_dl_ctx,
     color = "technique",
-    palette = {"pycisTarget": "#0D3B66", "deepPBMC": "#F95738"},
+    #palette = {"pycisTarget": "#0D3B66", "deepPBMC": "#F95738"},
     save = "_technique.png"
 )
 
 sc.tl.leiden(adata_motifs_dl_ctx, resolution=1)
+
+cluster_to_dbd = (
+    adata_motifs_dl_ctx.obs[["leiden", "dbd"]]
+        .groupby("leiden", observed=True)["dbd"]
+        .agg(lambda x: x.mode().iloc[0] if len(x.mode()) > 0 else np.nan)
+        .to_dict()
+)
+
+adata_motifs_dl_ctx.obs["cluster_dbd"] = [
+    cluster_to_dbd[cl] for cl in adata_motifs_dl_ctx.obs["leiden"]
+]
+
 sc.pl.tsne(
     adata_motifs_dl_ctx,
     color=["leiden"],
     save="_leiden.png",
     legend_loc="on data",
 )
+
+for cluster in adata_motifs_dl_ctx.obs["leiden"].unique():
+    print(cluster)
+    fig, axs = plt.subplots(figsize = (12, 2), ncols = 3)
+    for technique, ax in zip(
+            adata_motifs_dl_ctx.obs["technique"].unique(),
+            axs.ravel()):
+        motifs_of_cluster_and_tech = np.logical_and(
+            adata_motifs_dl_ctx.obs["leiden"] == cluster,
+            adata_motifs_dl_ctx.obs["technique"] == technique
+        )
+        if sum(motifs_of_cluster_and_tech) == 0:
+            continue
+        print(technique)
+        motifs = [
+            all_motifs[x] for x in np.where(motifs_of_cluster_and_tech)[0]
+        ]
+        ics = [
+            np.nan_to_num(ic(m.T)) for m in motifs
+        ]
+        motif_to_show = 0#np.argmax([x.sum() for x in ics])
+        _ = logomaker.Logo(
+            pd.DataFrame(
+                motifs[motif_to_show].T * ics[motif_to_show][:, None],
+                columns = list("ACGT")
+            ),
+            ax = ax
+        )
+    fig.savefig(f"plots/logos_cluster_{cluster}.pdf")
+    fig.savefig(f"plots/logos_cluster_{cluster}.png")
+
 
 corr_matrix = adata_motifs_dl_ctx.to_df().corr()
 
@@ -171,4 +270,151 @@ fig = sns.clustermap(
     xticklabels=False, yticklabels=False
 )
 fig.savefig("plots/heatmap_corr_matrix.png")
+
+leiden_color_dict = {
+    cluster: color
+    for cluster, color in zip(
+        adata_motifs_dl_ctx.obs["leiden"].unique(),
+        adata_motifs_dl_ctx.uns["leiden_colors"]
+    )   
+}
+
+technique_marker = {
+    "chromVAR": "^",
+    "deepPBMC": "o",
+    "pycisTarget": "."
+}
+
+technique_to_zorder = {
+    "chromVAR": 2,
+    "deepPBMC": 3,
+    "pycisTarget": 1
+}
+
+fig, ax = plt.subplots(figsize = (4,4))
+for technique, marker in technique_marker.items():
+    v_adata_motifs_dl_ctx = adata_motifs_dl_ctx[
+        adata_motifs_dl_ctx.obs["technique"] == technique
+    ]
+    ax.scatter(
+        v_adata_motifs_dl_ctx.obsm["X_tsne"][:, 0],
+        v_adata_motifs_dl_ctx.obsm["X_tsne"][:, 1],
+        c = [
+            leiden_color_dict[cl]
+            for cl in v_adata_motifs_dl_ctx.obs["leiden"]
+        ],
+        marker=marker,
+        label = technique,
+        edgecolors="black",
+        zorder=technique_to_zorder[technique]
+    )
+
+ax.legend()
+ax.set_axis_off()
+fig.tight_layout()
+fig.savefig("plots/tSNE_leiden_and_technique.png", dpi = 300)
+fig.savefig("plots/tSNE_leiden_and_technique.pdf")
+
+
+sc.pl.tsne(
+    adata_motifs_dl_ctx,
+    color = "cluster_dbd",
+    save = "_cluster_dbd.png"
+)
+
+dbd_color_dict = {
+    cluster: color
+    for cluster, color in zip(
+        adata_motifs_dl_ctx.obs["cluster_dbd"].unique().dropna(),
+        adata_motifs_dl_ctx.uns["cluster_dbd_colors"]
+    )   
+}
+
+dbd_color_dict[np.nan] = "gray"
+
+
+fig, ax = plt.subplots(figsize = (4,4))
+for technique, marker in technique_marker.items():
+    v_adata_motifs_dl_ctx = adata_motifs_dl_ctx[
+        adata_motifs_dl_ctx.obs["technique"] == technique
+    ]
+    ax.scatter(
+        v_adata_motifs_dl_ctx.obsm["X_tsne"][:, 0],
+        v_adata_motifs_dl_ctx.obsm["X_tsne"][:, 1],
+        c = [
+            dbd_color_dict[cl]
+            for cl in v_adata_motifs_dl_ctx.obs["cluster_dbd"]
+        ],
+        marker=marker,
+        label = technique,
+        edgecolors="black",
+        zorder=technique_to_zorder[technique]
+    )
+
+ax.legend()
+ax.set_axis_off()
+fig.tight_layout()
+fig.savefig("plots/tSNE_dbd_and_technique.png", dpi = 300)
+fig.savefig("plots/tSNE_dbd_and_technique.pdf")
+
+fig, ax = plt.subplots()
+for dbd, color in dbd_color_dict.items():
+    ax.scatter([], [], label = dbd, color = color)
+fig.legend()
+fig.tight_layout()
+fig.savefig("plots/dbd_legend.pdf")
+
+for dbd in ["HMG/Sox", "Homeodomain"]:
+    print(dbd)
+    for technique in adata_motifs_dl_ctx.obs["technique"].unique():
+        print(technique)
+        motifs_of_dbd_and_tech = np.logical_and(
+                adata_motifs_dl_ctx.obs["cluster_dbd"] == dbd,
+                adata_motifs_dl_ctx.obs["technique"] == technique
+            )
+        motifs = [
+            all_motifs[x] for x in np.where(motifs_of_dbd_and_tech)[0]
+        ]
+        ics = [
+            np.nan_to_num(ic(m.T)) for m in motifs
+        ]
+        for i, (motif_, ic_) in enumerate(zip(motifs, ics)):
+            print(f"{i + 1}/{len(motifs)}", end = "\r")
+            fig, ax = plt.subplots(figsize = (4, 2))
+            _ = logomaker.Logo(
+                pd.DataFrame(
+                    motif_.T * ic_[:, None],
+                    columns = list("ACGT")
+                ),
+                ax = ax
+            )
+            fig.savefig(f"plots/logo_{dbd.replace('/', '_')}_{technique}_{i}.png")
+            fig.savefig(f"plots/logo_{dbd.replace('/', '_')}_{technique}_{i}.pdf")
+
+for dbd in ["bZIP"]:
+    print(dbd)
+    technique = "chromVAR"
+    print(technique)
+    motifs_of_dbd_and_tech = np.logical_and(
+            adata_motifs_dl_ctx.obs["cluster_dbd"] == dbd,
+            adata_motifs_dl_ctx.obs["technique"] == technique
+        )
+    motifs = [
+        all_motifs[x] for x in np.where(motifs_of_dbd_and_tech)[0]
+    ]
+    ics = [
+        np.nan_to_num(ic(m.T)) for m in motifs
+    ]
+    for i, (motif_, ic_) in enumerate(zip(motifs, ics)):
+        print(f"{i + 1}/{len(motifs)}", end = "\r")
+        fig, ax = plt.subplots(figsize = (4, 2))
+        _ = logomaker.Logo(
+            pd.DataFrame(
+                motif_.T * ic_[:, None],
+                columns = list("ACGT")
+            ),
+            ax = ax
+        )
+        fig.savefig(f"plots/logo_{dbd.replace('/', '_')}_{technique}_{i}.png")
+        fig.savefig(f"plots/logo_{dbd.replace('/', '_')}_{technique}_{i}.pdf")
 
